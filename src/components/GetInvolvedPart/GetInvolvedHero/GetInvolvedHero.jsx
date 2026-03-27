d48854331645e5d230fe7a370549fca363705b8e
MDaniyal18 <sidiq.daniyal@gmail.com>
2026-03-27 10:33:25 +0530

    arrows updated

--- src/components/GetInvolvedPart/GetInvolvedHero/GetInvolvedHero.jsx
+++ src/components/GetInvolvedPart/GetInvolvedHero/GetInvolvedHero.jsx
@@ -1,12 +1,13 @@
 import "../../../styles/Get-Involved/getinvolved-hero.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Keyboard } from "swiper/modules";
+import { Pagination, Keyboard, Navigation } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-// ✅ NEW IMAGES
+// ✅ IMAGES
 import giImage1 from "../../../assets/Images/I-am.jpg";
 import giImage2 from "../../../assets/Images/getinvolved1.png";
 import giImage3 from "../../../assets/Images/getinvolved2.png";
@@ -16,16 +17,15 @@ import giImage6 from "../../../assets/Images/getinvolved5.png";
 import giImage7 from "../../../assets/Images/getinvolved6.png";
 import giImage8 from "../../../assets/Images/getinvolved7.png";
 import giImage9 from "../../../assets/Images/getinvolved8.png";
-// import giImage10 from "../../../assets/Images/getinvolved9.png";
 
 const GetInvolvedHero = () => {
   return (
     <section className="gi-hero" tabIndex={0}>
-
       <Swiper
-        modules={[Pagination, Keyboard]}
+        modules={[Pagination, Keyboard, Navigation]}
         loop={true}
         pagination={{ clickable: true }}
+        navigation={true} // ✅ arrows enabled
 
         keyboard={{
           enabled: true,
@@ -38,9 +38,8 @@ const GetInvolvedHero = () => {
         spaceBetween={0}
         className="gi-swiper"
       >
-
         {[
-          giImage1, 
+          giImage1,
           giImage2,
           giImage3,
           giImage4,
@@ -48,17 +47,19 @@ const GetInvolvedHero = () => {
           giImage6,
           giImage7,
           giImage8,
-          giImage9
+          giImage9,
         ].map((img, index) => (
           <SwiperSlide key={index}>
-  <div className="gi-slide">
-    <img src={img} alt={`slide-${index}`} className="gi-image" />
-  </div>
-</SwiperSlide>
+            <div className="gi-slide">
+              <img
+                src={img}
+                alt={`slide-${index}`}
+                className="gi-image"
+              />
+            </div>
+          </SwiperSlide>
         ))}
-
       </Swiper>
-
     </section>
   );
 };
